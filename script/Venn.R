ff356d48e166172ab5ad5aaac9c525fe0580238a
Valentin Goupille <125963826+vgoupille@users.noreply.github.com>
2024-11-20 17:06:23 +0100

    test

---
+++ script/Venn.R
@@ -0,0 +1,10 @@
+if (!requireNamespace("BiocManager"))
+    install.packages("BiocManager")
+`BiocManager::install("VennDetail")
+
+# Chargement des packages utilisés
+library(VennDetail)
+#read excel
+library(readxl)
+
+# Chargement des 3 fichiers de données
